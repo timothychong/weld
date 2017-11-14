ddea2885eeda5870c1e13df1be566b5813b165e9
Dennis Chui Wang <denniswang@Denniss-MacBook-Pro.local>
2017-11-13 21:59:44 -0800

    Added some more descriptive comments to libtest.cc

--- examples/cpp/vector_add_fpga/fpga/libtest.cc
+++ examples/cpp/vector_add_fpga/fpga/libtest.cc
@@ -3,27 +3,31 @@
 #include <inttypes.h>
 #include <iostream>
 
-// From prelude.ll:82
+// From prelude.ll:82, standard struct to represent a vector for Weld
 struct weld_vector_t {
 	int32_t *data;
 	int64_t length;
 };
 
+// Arguments for this particular function
 struct args_t {
 	struct weld_vector_t vector;
 	int32_t a;
 };
 
+// Standard wrapper for input arguments, data is a pointer to an args_t
 struct input_arg_t {
 	int64_t data;
 	int32_t n_workers;
 	int64_t mem_limit;
 };
 
+// Output result type for this particular function
 struct results_t {
 	struct weld_vector_t vector;
 };
 
+// Standard wrapper for output arguments, data is a pointer to a results_t
 struct output_result_t {
 	int64_t data;
 	int64_t run_id;
@@ -32,6 +36,7 @@ struct output_result_t {
 
 extern "C" int64_t test_run_method_name(int64_t in)
 {
+	// Parse input arguments
 	struct args_t *args = (struct args_t *)(((input_arg_t *)in)->data);
 	int64_t vec_len = args->vector.length;
 	int32_t *in_vec = args->vector.data;
